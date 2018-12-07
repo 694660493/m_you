575de2caf79c894decb79daed5c1057336459a21
zzx <694660493@qq.com>
2018-12-07 16:39:41 +0800

    cutting hair

---
+++ src/components/notfount/notfount.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>页面跑到月球去了</p>
+    <router-link to="/">返回首页</router-link>
+  </div>
+
+</template>
+
+<script>
+    export default {
+        name: "notfount"
+    }
+</script>
+
+<style scoped lang="stylus">
+    div
+      position relative
+      font-size 1rem
+      p
+        color: lime
+      a
+        position absolute
+        top 2rem
+        border 1px solid black
+        border-radius 10px
+
+</style>
